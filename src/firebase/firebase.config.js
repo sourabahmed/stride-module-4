0e376acaad7b3ee7f3d37546c16471fba01c2204
unknown <you@example.com>
2024-05-20 23:07:13 +0100

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY1w_1LLg8-XFEuXnKJurodR5EWIQQKLw",
+  authDomain: "stride-practice-d655b.firebaseapp.com",
+  projectId: "stride-practice-d655b",
+  storageBucket: "stride-practice-d655b.appspot.com",
+  messagingSenderId: "995672820857",
+  appId: "1:995672820857:web:585be0abe93fd49fd418a8"
+};
+
+export const app = initializeApp(firebaseConfig);
